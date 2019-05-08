2b13bcb4667d124d502acb7ee4ebddb7e0dd1772
Harshavardhan.Kadiyala <devkhv129@gmail.com>
2019-05-07 23:59:54 -0700

    error handling

--- profiler_test.py
+++ profiler_test.py
@@ -2,31 +2,30 @@ import unittest
 from profiler import profiler
 
 class profiler_test(unittest.TestCase):
+	def setUp(self):
+		self.prof = profiler()
     def test_success_case(self):
-        prof = profiler()
         filename = "test.txt"
         throughput_values = [1,2,3,4,5,6,7]
         latency_values = [1,2,3,4,5,6,7]
         errors = [1,2,3,4,5,6,7]
         values = [1,2,3,4,5,6,7]
-        self.assertTrue(prof.reporter(filename,throughput_values,latency_values,errors,values))
+        self.assertTrue(self.prof.reporter(filename,throughput_values,latency_values,errors,values))
 
     def test_latency_bad_data_case(self):
-        prof = profiler()
         filename = "test.txt"
         throughput_values = [1,2,3,4,5,6,7]
         latency_values = [3,4,5,6,7]
         errors = [1,2,3,4,5,6,7]
         values = [1,2,3,4,5,6,7]
-        self.assertTrue(prof.reporter(filename,throughput_values,latency_values,errors,values))
+        self.assertTrue(self.prof.reporter(filename,throughput_values,latency_values,errors,values))
 
     def test_value_bad_data_case(self):
-        prof = profiler()
         filename = "test.txt"
         throughput_values = [1,2,3,4,5,6,7]
         latency_values = [1,2,3,4,5,6,7]
         errors = [1,2,3,4,5,6,7]
         values = [3,4,5,6,7]
-        self.assertTrue(prof.reporter(filename,throughput_values,latency_values,errors,values))
+        self.assertTrue(self.prof.reporter(filename,throughput_values,latency_values,errors,values))
 if __name__ == '__main__':
     unittest.main()
